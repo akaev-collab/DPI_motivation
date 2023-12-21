29f327db134fc92d99a9c92439fcdf28cc1dfefd
Dzhakhar Akaev <dzhakhar.akaev@gmail.com>
2023-12-21 22:21:19 +0300

    delete altair chart

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,9 @@ import json
 import altair as alt
 import datetime as dt
 
+import plotly.express as px
+import plotly.graph_objects as go
+
 st.set_page_config(layout="wide")
 st.title("Дашборд")
 
@@ -48,86 +51,108 @@ tabel_1, tabel_2, tabel_3 = st.tabs(["Готовность", "Качество",
 
 with tabel_1:
     st.header("Готовность документации")
-    df_load_group = df_load_group[df_load_group["Мастерская"].isin(filter_selected_master)]
-    df_load_plan = df_load_group.groupby([df_load_group["Дата"].dt.strftime("%Y-%m-1")]).aggregate({"План":"sum"}).reset_index()
-    df_load_fact = df_load_group.groupby([df_load_group["Дата"].dt.strftime("%Y-%m-1")]).aggregate({"Факт":"sum"}).reset_index()
-
-    bar_load_plan = alt.Chart(df_load_plan).mark_bar(width=22, xOffset=-10, color="grey").encode(
-        alt.X("Дата:T", axis=alt.Axis(format="%m.%Y")),
-        y = "План", 
-        tooltip=["Дата:T", "План"]
-    ).properties(width=chart_width, height=chart_height)
-
-    bar_load_fact = alt.Chart(df_load_fact, title = "Динамика выдачи комплектов").mark_bar(width=22, xOffset=10, color = "#007FFF").encode(
-        alt.X("Дата:T"),
-        y = "Факт",
-        tooltip=["Дата:T", "Факт"]
-    ).properties(width=chart_width, height=chart_height)
-
-    df_load_fact_total = df_load_group.groupby(["Мастерская", "Статус по выдаче"], as_index=False).aggregate({"Факт":"sum"})
-
-    bar_load_fact_total = alt.Chart(df_load_fact_total, title = "Статус по выданым комплектам").mark_bar().encode(
-        alt.X("Факт", title=""),
-        alt.Y("Мастерская", title=""),
-        color = "Статус по выдаче"
-    ).properties(width=chart_width, height=chart_height)
-    st.altair_chart(bar_load_plan + bar_load_fact)
-    st.altair_chart(bar_load_fact_total)
     
+    df_load_group = df_load_group[df_load_group["Мастерская"].isin(filter_selected_master)]
+
+    line_data = df_load_group.groupby(["Мастерская", "Статус по выдаче", df_load_group["Дата"].dt.strftime("%Y-%m-1")]).aggregate({"План":"sum", "Факт":"sum"}).reset_index()
+
+    chart_line = line_data.groupby("Дата").aggregate({"План":"sum", "Факт":"sum"}).reset_index()
+    chart_master_status = line_data.groupby(["Дата", "Мастерская"]).aggregate({"План":"sum", "Факт":"sum"}).reset_index()
+    chart_status = line_data.groupby(["Дата", "Статус по выдаче"]).aggregate({"План":"sum", "Факт":"sum"}).reset_index()
+
+    figure_line = go.Figure()
+
+    figure_line.add_trace(go.Scatter(x = chart_line["Дата"], 
+                                     y = chart_line["Факт"], 
+                                     name = "Факт", 
+                                     mode = "lines + markers", 
+                                     marker_color = "#007FFF"))
+    
+    
+    figure_line.add_trace(go.Scatter(x = chart_line["Дата"], 
+                                     y = chart_line["План"], 
+                                     name = "План", 
+                                     mode = "lines + markers", 
+                                     marker_color = "grey"))
+
+    figure_line.update_layout(
+                    title = "Статус по выдаче комплектов",
+                    width=1500,
+                    height=500,
+                    xaxis_title='Месяц',
+                    yaxis_title='Значение', 
+                    legend_orientation="h",
+                    legend=dict(x=.5, xanchor="center"),
+                    margin=dict(l=0, r=0, t=0, b=0))
+
+
+
+    figure_master = px.bar(chart_status, 
+                           x = "Дата", 
+                           y = "Факт", 
+                           color="Статус по выдаче",
+                           title="Статус по выдаче комплектов")
+    
+    figure_status = px.bar(chart_master_status, 
+                           x = "Дата", 
+                           y = "Факт", 
+                           color="Мастерская",
+                           title = "Статус по выдаче комплектов по мастерским")
+
+    st.plotly_chart(figure_line)
+
+    chart_3, chart_4  = st.columns(2)
+
+    with chart_3:
+        st.plotly_chart(figure_master)
+    with chart_4:
+        st.plotly_chart(figure_status)
+    
+
 with tabel_2:
     st.header("Качество документации")
     
     df_izm_group = df_izm_group[df_izm_group["Мастерская"].isin(filter_selected_master)]
-    df_izm_group = df_izm_group.round({"% ИЗМ":2})
 
-    chart_izm = alt.Chart(df_izm_group).mark_line(width=18).encode(
-        alt.X("Дата"),
-        alt.Y("% ИЗМ"),
-        color="Мастерская"
-    ).properties(width=800, height=400)
-
-    nearest = alt.selection(type='single', nearest=True, on='mouseover',
-                        fields=['Дата'], empty='none')
-
-    selectors = alt.Chart(df_izm_group).mark_point().encode(
-        x='Дата',
-        opacity=alt.value(0),
-    ).add_selection(nearest)
-
-    points = chart_izm.mark_point(size=70).encode(
-        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
-    )
-
-    text = chart_izm.mark_text(align='left', dx=25, dy=-25).encode(
-    text=alt.condition(nearest, '% ИЗМ:Q', alt.value(' ')))
-        
-    rules = alt.Chart().mark_rule(color='gray').encode(
-    x='Дата:T',
-    ).transform_filter(
-    nearest
-    )
-
-    st.altair_chart((chart_izm + selectors + points + text + rules).interactive())
+    figure_izm_status = px.bar(df_izm_group,
+                               x = "Дата",
+                               y = "% ИЗМ",
+                               color = "Мастерская",
+                               width=1500,
+                               height=500)
+    
+    st.plotly_chart(figure_izm_status)
 
 with tabel_3:
     st.header("Продуктивность мастерских")
     
     df_productivity_group = df_productivity_group[df_productivity_group["Мастерская"].isin(filter_selected_master)]
 
-    df_productivity_plan = df_productivity_group.groupby([df_productivity_group["Дата"].dt.strftime("%Y-%m-1")]).aggregate({"План":"sum"}).reset_index().round({"План":2})
+    productivity_data = df_productivity_group.groupby([df_productivity_group["Дата"].dt.strftime("%Y-%m-1")]).aggregate({"План":"sum","Факт":"sum"}).reset_index().round({"План":2})
 
-    df_productivity_fact = df_productivity_group.groupby([df_productivity_group["Дата"].dt.strftime("%Y-%m-1")]).aggregate({"Факт":"sum"}).reset_index().round({"Факт":2})
+    
+    figure_productivity_line = go.Figure()
+    
+    figure_productivity_line.add_trace(go.Scatter(x = productivity_data["Дата"], 
+                                     y = productivity_data["Факт"], 
+                                     name = "Факт",
+                                     marker_color = "#007FFF"))
+    
+    
+    figure_productivity_line.add_trace(go.Scatter(x = productivity_data["Дата"], 
+                                     y = productivity_data["План"], 
+                                     name = "План",  
+                                     marker_color = "grey"))
+    
+    figure_productivity_line.update_layout(
+                    title = "Выработка",
+                    width=1500,
+                    height=500,
+                    xaxis_title='Месяц',
+                    yaxis_title='Значение', 
+                    legend_orientation="h",
+                    legend=dict(x=.5, xanchor="center"),
+                    margin=dict(l=0, r=0, t=0, b=0))
 
-    bar_productivity_plan = alt.Chart(df_productivity_plan).mark_bar(width=22, xOffset=-10, color="grey").encode(
-        alt.X("Дата:T", axis=alt.Axis(format="%m.%Y")),
-        y = "План", 
-        tooltip=["Дата:T", "План"]
-    ).properties(width=chart_width, height=chart_height)
 
-    bar_productivity_fact = alt.Chart(df_productivity_fact).mark_bar(width=22, xOffset=10, color = "#007FFF").encode(
-        alt.X("Дата:T"),
-        y = "Факт",
-        tooltip=["Дата:T", "Факт"]
-    ).properties(width=chart_width, height=chart_height)
-
-    st.altair_chart(bar_productivity_plan + bar_productivity_fact)
+    st.plotly_chart(figure_productivity_line)
